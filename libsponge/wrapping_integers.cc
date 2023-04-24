38fac4d7501b8801a460695c0048aeae1d3a625e
Hestar4869 <3341238768@qq.com>
2023-04-24 16:13:11 +0800

    feat:finish the lab2-1
    finish the part of the wrapping_integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,12 +1,13 @@
 #include "wrapping_integers.hh"
 
+#include <cmath>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -14,8 +15,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    const uint64_t MAX_LEN = 1ul << 32;
+    auto nn = static_cast<uint32_t>((n + isn.raw_value()) % MAX_LEN);
+    return WrappingInt32(nn);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const uint64_t MAX_LEN = 1ul << 32;
+    uint32_t offset = n.raw_value() - isn.raw_value();
+    uint64_t nn = checkpoint - (checkpoint % MAX_LEN) + offset;
+    if (nn < checkpoint)
+        nn = checkpoint - nn < nn + MAX_LEN - checkpoint ? nn : nn + MAX_LEN;
+    else if(nn >= MAX_LEN)
+        nn = nn - checkpoint < checkpoint - nn + MAX_LEN ? nn : nn - MAX_LEN;
+
+    return nn;
 }
